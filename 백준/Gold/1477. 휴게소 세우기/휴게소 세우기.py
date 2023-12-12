cd55e16e405c409ca47febdd34297dfa8e94ab82
Bin-Choi <109321881+Bin-Choi@users.noreply.github.com>
2023-12-12 10:36:59 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -0,0 +1,22 @@
+N, M, L = map(int, input().split())
+arr = [0] + list(map(int, input().split())) + [L]
+arr.sort()
+
+s, e = 1, L
+ans = 0
+
+while s <= e:
+    cnt = 0
+    mid = (s+e) // 2
+
+    for i in range(1, len(arr)):
+        if arr[i] - arr[i-1] > mid:
+            cnt += (arr[i] - arr[i-1] - 1) // mid
+
+    if cnt > M:
+        s = mid + 1
+    else:
+        e = mid - 1
+        ans = mid
+
+print(ans)
