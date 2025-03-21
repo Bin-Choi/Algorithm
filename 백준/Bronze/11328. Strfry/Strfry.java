8fc29c1a7e77c3d149f174a8e72d1e9cb0bfba01
Bin-Choi <csb5268@gmail.com>
2025-03-21 09:58:33 +0900

    [Bronze II] Title: Strfry, Time: 304 ms, Memory: 20972 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -3,33 +3,54 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
+        for (int t = 0; t < N; t++) { // ✅ i 대신 t 사용하여 반복문 안정화
+            String line = br.readLine();
 
-
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            String str = st.nextToken();
-            String target = st.nextToken();
-            
-            char[] chArr1 = str.toCharArray();
-            Arrays.sort(chArr1);
-
-            String origin = new String(chArr1);
-
-            chArr1 =target.toCharArray();
-            Arrays.sort(chArr1);
-            
-            String compair = new String(chArr1);
-
-            if (origin.equals(compair)) {
-                System.out.println("Possible");
-            } else {
+            // ✅ 입력이 없을 경우 방어 코드 추가
+            if (line == null || line.isEmpty()) {
                 System.out.println("Impossible");
+                continue;
             }
+
+            StringTokenizer st = new StringTokenizer(line);
+
+            // ✅ 두 개의 문자열이 존재하지 않으면 Impossible 출력
+            if (st.countTokens() != 2) {
+                System.out.println("Impossible");
+                continue;
+            }
+
+            String a = st.nextToken();
+            String b = st.nextToken();
+
+            // 길이가 다르면 Impossible
+            if (a.length() != b.length()) {
+                System.out.println("Impossible");
+                continue;
+            }
+
+            int[] arr = new int[26]; 
+            boolean flag = true;
+            int n = a.length();
+
+            // ✅ i를 반복문 안에서만 사용하도록 변경
+            for (int i = 0; i < n; i++) {
+                arr[a.charAt(i) - 'a']++;
+                arr[b.charAt(i) - 'a']--;
+            }
+
+            // ✅ 알파벳 카운트가 0이 아닌 것이 있다면 Impossible
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] != 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            System.out.println(flag ? "Possible" : "Impossible");
         }
     }
 }
