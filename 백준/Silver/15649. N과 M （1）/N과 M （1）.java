4e57cff174c7c0160c8ac54c298fb05ccbeced5b
Bin-Choi <csb5268@gmail.com>
2025-04-29 09:46:43 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 23412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,41 +1,48 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    static int n, m;
-    static int[] arr;
-    static boolean[] visited;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        arr = new int[m+1];
-        visited = new boolean[n+1];
-        recursion(0);
-   }
 
-    private static void recursion(int depth) {
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(arr[i] + " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    
+    public static void dfs(int k) {
+
+        if(k == M) {
+            for (int v : arr) {
+                sb.append(v).append(' ');
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i;
-                recursion(depth + 1);
-                visited[i] = false;
+        for (int i = 0; i < N; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                arr[k] = i+1;
+                dfs(k+1);
+                visit[i] = false;
             }
         }
-   }
+    }
 }
