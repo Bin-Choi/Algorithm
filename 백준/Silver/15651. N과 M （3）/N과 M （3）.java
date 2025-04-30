2d6934c39fc91a99923ba312a8f15313b1479f53
Bin-Choi <csb5268@gmail.com>
2025-04-30 09:28:42 +0900

    [Silver III] Title: N과 M (3), Time: 404 ms, Memory: 69052 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+
+    public static void dfs(int depth) {
+
+        if(depth == M) {
+            for (int v : arr) {
+                sb.append(v).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1); 
+        }
+    }
+}
